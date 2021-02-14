e2403b9e9a286b288d5600dc552510f4ad852013
Santiago Rincon <santiagorincon2001@gmail.com>
2021-02-14 18:15:38 -0500

    Utilizacion de un formulario con dos entradas Guia de https://es.reactjs.org/docs/forms.html

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Reservas></Reservas>
     </div>
+  );
+}
+function Reservas(props){
+  const [ciudad, setCiudad] = useState("bogota")
+  const [dia , setDia] = useState("1")
+
+
+  function handleSubmit(event){
+    alert(`se a enviado el contenido! ${ciudad} el dia ${dia}`)
+    event.preventDefault();
+  }
+
+  function handleChangeCiudad(event){
+    setCiudad(event.target.value)
+  }
+  function handleChangeDia(event){
+    setDia(event.target.value)
+  }
+  
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Selecciona la ciudad:
+        <select value={ciudad} onChange={handleChangeCiudad}>
+          <option value="bogota">Bogota</option>
+          <option value="medellin">Medellin</option>
+          <option value="barranquilla">Barranquilla</option>
+          <option value="cali">Cali</option>
+        </select>
+      </label>
+      <input value={dia} type="text" placeholder="un numero" onChange={handleChangeDia}/>
+      <input type="submit" value="Submit" />
+    </form>
   );
 }
 
